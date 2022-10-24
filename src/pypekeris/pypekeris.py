58b93d55c882cfd393529157ec64eb5ee1279162
Gilaverbuch <gilaverbuch@gmail.com>
2022-10-24 11:17:24 -0700

    added pekeris broadband

--- src/pypekeris/pypekeris.py
+++ src/pypekeris/pypekeris.py
@@ -23,6 +23,8 @@ plt.rcParams['figure.facecolor'] = 'white'
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from obspy import read, UTCDateTime, Stream, Trace
 
 from .core.core import pekeris_
 
@@ -69,6 +71,76 @@ def pekeris(zs=10, zr=10, r=5000, dr=10, c0=1500,  c1=1500,  c2=2000, d=50, dz=0
 
 # -------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------
+
+
+
+def pekeris_broadband(t0=3, t_max=50, fmin=1e1, fmax=1e2, dt=1e-3, zs=5, zr=5, r=1e4, d=20, num_mode=3):
+    '''
+    This function is based on the Pekeris class, and conputes the broadband signal in a Pekeris waveguide.
+    
+    parameters
+    ----------
+        t0 = time delay of the signal, default is 3 sec
+        t_max = maximum simulation time, default is 50 sec 
+        fmin = minimum frequency, default is 1e1 Hz 
+        fmax = maximum frequency, default is 1e2 Hz 
+        dt = time intevals, default is 1e-3 sec 
+        zs = source depth, default is 5 m 
+        zr = receiver depth, default is 5 m 
+        r = receiver distance, default is 1e4 m
+        d = waveguide depth, default is 20 m
+        num_mode = number of modes to compute, default is 3
+    '''
+    
+    l=int(t_max/dt)
+    time = np.arange(0,dt*l, dt)
+
+    t0_samp = int(t0/dt)
+
+    sig_ref = np.zeros(time.size, dtype=np.float32)
+    sig_ref[t0_samp] = 1
+
+    tr_s = Trace()
+    tr_s.stats.network = 'Signal'
+    tr_s.stats.station = 'src'
+    tr_s.stats.channel = 'FDH' # 
+    tr_s.stats.starttime = UTCDateTime.now()
+    tr_s.stats.sampling_rate = 1/dt
+    tr_s.stats.delta = dt
+    tr_s.stats.npts = time.size
+    tr_s.stats.calib = 1
+    tr_s.stats.units = 'Norm.'
+    tr_s.data = sig_ref
+
+    tr_s.filter('bandpass', freqmin=fmin, freqmax=fmax, corners=4)
+
+    sig_ref = tr_s.data/np.max(tr_s.data)
+
+    l = len(sig_ref)
+    frequency = np.fft.fftfreq(l, dt)
+    df = frequency[1]
+    freq_ref = np.fft.fft(sig_ref)
+
+    pressure = np.zeros(frequency.size, dtype=np.complex64)
+
+    fmax_idx = int(frequency.size/2)
+    for idx, f in enumerate(tqdm(frequency[1:fmax_idx])):
+        P = pekeris(f=f, nq=5e4, dr=1, zs=1, d=20)
+        P._calc_parameters()
+        P._calc_field(r_rec=r, z_rec=zr, num_mode=num_mode)
+        pressure[idx] = P.Phi*freq_ref[idx]
+        
+    pressure_time = np.real(np.fft.fft(freq_ref*pressure*2))
+    T = r/P.c1
+    shift_samp = int(T/dt)
+    signal = np.roll(pressure_time, shift=shift_samp)
+    
+    tr_r = tr_s.copy()
+    tr_r.stats.station = 'rcv'
+    tr_r.data = signal
+
+    
+    return tr_s, tr_r
 
 
 
